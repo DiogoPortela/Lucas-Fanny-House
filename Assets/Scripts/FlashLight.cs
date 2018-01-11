ce5acbb88239f31a40d9f30f51f0766a759dd4ea
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2018-01-11 03:39:56 +0000

    hella lotta shiat

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour {
+
+    public GameObject light;
+    private bool isOn = true;
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isOn = !isOn;
+            light.SetActive(isOn);
+        }
+	}
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject bulletPrefab;
+    public float shootingForce;
+    public GameObject parent;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, target.transform.position, target.transform.rotation, parent.transform);
+            Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+            rb.AddForce(target.transform.up * shootingForce, ForceMode.Impulse);
+        }
+	}
+
+}
