ad724ec7a2393b0ea36fd0304ffe33e011f70a71
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2018-01-12 02:54:16 +0000

    Added lights and shit

---
+++ Assets/Scripts/Flickering.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour {
+
+    public float flicker = 0.5f;
+    public Light light;
+
+	// Update is called once per frame
+	void Update () {
+        float newRandom = Random.value;
+
+        if(newRandom > flicker)
+        {
+            light.enabled = !light.enabled;
+        }
+    }
+}
